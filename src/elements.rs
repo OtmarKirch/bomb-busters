f68cada97f0b3bde8bf833b3a30b700ae3e9fd47
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-06-05 11:44:20 +0200

    Add test_init_cables_in_game

--- src/elements.rs
+++ src/elements.rs
@@ -1,8 +1,11 @@
 //! This module contains all the elements the game uses, such as cables and equipment.
 //! Note that cable values are represented as integers. A cable in the game with the number 4.5 is represented as 45 in the code. This is to avoid floating point precision issues.
+//! The collections should be initialized once immutably at the start of the game with the init functions. Other functions refer to these collections by unique IDs.
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::sync::atomic::{AtomicU32, Ordering};
+use rand::prelude::*;
+use rust_helpers::{split_rand_vec, split_rand_vec_eq};
 
 static CABLE_ID_COUNTER: AtomicU32 = AtomicU32::new(1);
 
@@ -18,7 +21,7 @@ static CABLE_ID_COUNTER: AtomicU32 = AtomicU32::new(1);
 fn init_all_cables(blue_max_val: u32, red_show: u32, yellow_show: u32) -> HashMap<u32, u32> {
     let mut cables = HashMap::new();
     for i in 1..=blue_max_val {
-        for j in 1..=4 {
+        for _ in 1..=4 {
             cables.insert(CABLE_ID_COUNTER.fetch_add(1, Ordering::Relaxed), i as u32 * 10);
         }
     }
@@ -36,12 +39,13 @@ fn init_all_cables(blue_max_val: u32, red_show: u32, yellow_show: u32) -> HashMa
     return cables
 }
 
-fn select_cable(
-    cables: &HashMap<u32, f32>,
+fn init_cables_in_game(
+    cables: &HashMap<u32, u32>,
     red_keep: u32,
     yellow_keep: u32,
-) -> (HashMap<u32, f32>, HashMap<u32, f32>) {
-    todo!()
+) -> HashSet<u32> {
+    todo!();
+
 }
 
 #[cfg(test)]
@@ -64,6 +68,22 @@ mod tests {
         assert_eq!(num_blue_cables, 4 * blue_max_val as usize, "Expected {} blue cables, found {}", 4 * blue_max_val, num_blue_cables);
         assert_eq!(num_red_cables, red_show as usize, "Expected {} red cables, found {}", red_show, num_red_cables);
         assert_eq!(num_yellow_cables, yellow_show as usize, "Expected {} yellow cables, found {}", yellow_show, num_yellow_cables);
+    }
+
+    #[test]
+    fn test_init_cables_in_game() {
+        let cables = init_all_cables(12, 2, 3);
+        let (red_keep, yellow_keep) = (1, 2);
+        let in_game_cables = init_cables_in_game(&cables, red_keep, yellow_keep);
+        let expected_cables = 4 * 12 + red_keep + yellow_keep;
+        assert_eq!(in_game_cables.len(), expected_cables as usize, "Expected {} cables in game, found {}", expected_cables, in_game_cables.len());
+
+        let num_blue_cables = in_game_cables.iter().filter(|&&id| cables[&id] % 10 == 0).count();
+        let num_red_cables = in_game_cables.iter().filter(|&&id| cables[&id] % 10 == 5).count();
+        let num_yellow_cables = in_game_cables.iter().filter(|&&id| cables[&id] % 10 == 1).count();
+        assert_eq!(num_blue_cables, 4 * 12, "Expected {} blue cables in game, found {}", 4 * 12, num_blue_cables);
+        assert_eq!(num_red_cables, red_keep as usize, "Expected {} red cables in game, found {}", red_keep, num_red_cables);
+        assert_eq!(num_yellow_cables, yellow_keep as usize, "Expected {} yellow cables in game, found {}", yellow_keep, num_yellow_cables);
     }
 }
 
