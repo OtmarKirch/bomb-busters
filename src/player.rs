ea5c24efd08f150a6cd31e7e17c450ac7f2ee196
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-06-05 16:12:46 +0200

    Add get_teammate_hands

--- src/player.rs
+++ src/player.rs
@@ -5,15 +5,6 @@ use rust_helpers::{split_rand_hashset_eq};
 
 use crate::elements::{init_all_cables, init_cables_in_game, get_value, get_color};
 
-static PLAYER_ID_COUNTER: AtomicU32 = AtomicU32::new(1);
-
-pub struct Player {
-    pub id: u32, // This is the player's ID
-    pub hand: Vec<u32>, // Assuming cards are represented by u32 IDs
-}
-
-fn init_player(number_players: u32){}
-
 fn init_cable_distribution(in_game_cables: &HashSet<u32>, number_players: u32) -> Vec<Vec<u32>> {
     let mut hands: Vec<Vec<u32>> = split_rand_hashset_eq(in_game_cables.clone(), number_players as usize);
 
@@ -28,12 +19,13 @@ fn sort_cable_distribution(hands: &mut Vec<Vec<u32>>, all_cables: &HashMap<u32, 
     }
 }
 
+#[derive(Clone, Debug)]
 pub struct Hand {
     cables: Vec<u32>, 
     status: Vec<CableStatus> 
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 enum CableStatus {
     Hidden,
     Clue,
@@ -47,6 +39,19 @@ fn init_hands(sorted_cable_distribution: Vec<Vec<u32>>) -> Vec<Hand> {
             status: vec![CableStatus::Hidden; cables.len()],
         }
     }).collect()
+}
+
+pub fn get_teammate_hands(player_number: u32, hands: &Vec<Hand>) -> Vec<Hand> {
+    let teammate_hands: Vec<Hand> = hands.iter()
+        .enumerate()
+        .filter(|(index, _)| *index as u32 != player_number)
+        .map(|(_, hand)| hand.clone())
+        .collect();
+
+    for hand in &teammate_hands {       
+    println!("Teammate's hand {:?}", hand);
+    }
+    return teammate_hands
 }
 
 
@@ -68,12 +73,12 @@ mod tests {
     fn test_sort_cable_distribution() {
         let all_cables = init_all_cables(10, 3, 2);
         let in_game_cables = init_cables_in_game(&all_cables, 2, 1);
-        let mut hands: Vec<Vec<u32>> = init_cable_distribution(&in_game_cables, 3);
-        assert!(!hands.is_empty(), "Hands should not be empty");
+        let mut cable_distributions: Vec<Vec<u32>> = init_cable_distribution(&in_game_cables, 3);
+        assert!(!cable_distributions.is_empty(), "Hands should not be empty");
 
-        sort_cable_distribution(&mut hands, &all_cables);
-        for hand in &hands {
-            let values: Vec<u32> = hand.iter().map(|id| get_value(*id, &all_cables)).collect();
+        sort_cable_distribution(&mut cable_distributions, &all_cables);
+        for cable_distribution in &cable_distributions {
+            let values: Vec<u32> = cable_distribution.iter().map(|id| get_value(*id, &all_cables)).collect();
             assert!(values.is_sorted(), "Hand should be sorted by values");
         }
     }
@@ -82,17 +87,34 @@ mod tests {
     fn test_init_hands() {
         let all_cables = init_all_cables(10, 3, 2);
         let in_game_cables = init_cables_in_game(&all_cables, 2, 1);
-        let hands: Vec<Vec<u32>> = init_cable_distribution(&in_game_cables, 3);
-        assert!(!hands.is_empty(), "Hands should not be empty");
+        let cable_distributions: Vec<Vec<u32>> = init_cable_distribution(&in_game_cables, 3);
+        assert!(!cable_distributions.is_empty(), "Hands should not be empty");
 
-        let mut sorted_hands = hands.clone();
+        let mut sorted_hands = cable_distributions.clone();
         sort_cable_distribution(&mut sorted_hands, &all_cables);
         let initialized_hands = init_hands(sorted_hands);
         
-        assert_eq!(initialized_hands.len(), hands.len(), "Number of hands should match");
+        assert_eq!(initialized_hands.len(), cable_distributions.len(), "Number of cable_distributions should match");
         for hand in initialized_hands {
             assert_eq!(hand.status.len(), hand.cables.len(), "Status length should match cables length");
             assert!(hand.status.iter().all(|s| matches!(s, CableStatus::Hidden)), "All cables should start as Hidden");
         }
+    }
+
+    #[test]
+    fn test_get_teammate_hands() {
+        let all_cables = init_all_cables(10, 3, 2);
+        let in_game_cables = init_cables_in_game(&all_cables, 2, 1);
+        let cable_distributions: Vec<Vec<u32>> = init_cable_distribution(&in_game_cables, 3);
+        assert!(!cable_distributions.is_empty(), "Hands should not be empty");
+
+        let mut sorted_hands = cable_distributions.clone();
+        sort_cable_distribution(&mut sorted_hands, &all_cables);
+        let initialized_hands = init_hands(sorted_hands);
+        
+        assert_eq!(initialized_hands.len(), cable_distributions.len(), "Number of cable_distributions should match");
+        let current_player = 1;
+
+        get_teammate_hands(current_player, &initialized_hands);
     }
 }
