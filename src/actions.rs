8d3f867d9e9bf73becfac030da7cc694b3e5911d
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2025-06-06 21:47:11 +0200

    Add test for duo_cut

--- src/actions.rs
+++ src/actions.rs
@@ -15,6 +15,10 @@ pub fn hint_random(hands: &mut Vec<Hand>) {
     }
 }
 
+pub fn duo_cut(player: u32, position_self: u32, position_teammate: u32, hands: &mut Vec<Hand>, all_cables: &HashMap<u32, u32>) {
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -32,5 +36,35 @@ mod tests {
             
             assert!(cnt_clue == 1, "Expected exactly one cable to be changed in player {}'s hand, caught {}", i, cnt_clue);
         }
+    }
+
+    #[test]
+    fn test_duo_cut() {
+        // Successfull duo cut
+        let all_cables = HashMap::from([
+            (1, 10), (2, 20), (3, 30), (4, 40), (5, 50), (6, 40), (7, 20), // blue
+            (8, 15), (9, 25), // red
+            (11, 11), (12, 21), (13, 31), // yellow
+        ]);
+        let mut hands = vec![
+            Hand::new(vec![1, 2, 3, 5, 6, 8, 11]),
+            Hand::new(vec![4, 7, 9, 10, 12, 13]),
+        ];
+
+        let hand0_cables = get_cables(&hands[0]);
+        let hand0_status = get_status(&hands[0]);
+        let hand1_cables = get_cables(&hands[1]);
+        let hand1_status = get_status(&hands[1]);
+
+        assert!(matches!(hand0_status[2], CableStatus::Hidden | CableStatus::Clue), "Expected cable at position 2 in player 0's hand to be hidden or a clue before duo cut");
+        assert!(matches!(hand1_status[2], CableStatus::Hidden | CableStatus::Clue), "Expected cable at position 2 in player 1's hand to be hidden or a clue before duo cut");
+        
+        duo_cut(0, 2, 2, &mut hands, &all_cables);
+        assert_eq!(hand0_status[2], CableStatus::Revealed, "Expected cable at position 2 in player 0's hand to be revealed after successfull duo cut");
+        assert_eq!(hand1_status[2], CableStatus::Revealed, "Expected cable at position 2 in player 1's hand to be revealed after successfull duo cut");
+        
+        // Unsuccessful duo cut
+
+        
     }
 }
